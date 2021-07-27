06e2dee801caf99bb9f0c78efb4157fc0c8dc07f
Nestsiarovich Uladzimir <winner20021991@mail.ru>
2021-07-27 19:11:25 +0300

    Update work - JS_7

--- JS_7/script.js
+++ JS_7/script.js
@@ -174,49 +174,49 @@ console.log('');
 
 // HW 7 ( JS )
 
-// let Lightbulb = function (name) {
+let Lightbulb = function (name) {
  
-//     this.get = function () {
-//         this.name = name;
-//         this.power = +prompt('Введите мощность лампочки, Вт');
-//         this.costEnergy = 0.209; //+prompt('Введите стоимость электроэнергии за 1Квт' + '/' + 'ч');
-//         if (confirm('Включить лампочку?')) {
-//             this.workTime = +prompt('Сколько времени работает лампочка? , ч');
-//         } else {
-//             this.workTime = 0;
-//         };
+    this.get = function () {
+        this.name = name;
+        this.power = +prompt('Введите мощность лампочки, Вт');
+        this.costEnergy = 0.209; //+prompt('Введите стоимость электроэнергии за 1Квт' + '/' + 'ч');
+        if (confirm('Включить лампочку?')) {
+            this.workTime = +prompt('Сколько времени работает лампочка? , ч');
+        } else {
+            this.workTime = 0;
+        };
  
-//         this.operation();
-//     };
+        this.operation();
+    };
  
-//     this.operation = function () {
-//         this.result = (this.power / 1000) * this.costEnergy * this.workTime;
+    this.operation = function () {
+        this.result = (this.power / 1000) * this.costEnergy * this.workTime;
  
-//         this.show();
-//     };
+        this.show();
+    };
  
-//     this.show = function () {
+    this.show = function () {
  
-//         if (this.workTime == 0 || this.workTime == undefined) {
-//             console.log('Лампочка "' + this.name + '" не включена');
-//         } else {
-//             console.log('Лампочка "' + this.name + '" проработала - ' + this.workTime + 'ч, и стоимость потраченой электроэнергии составляет = ' + this.result + 'р.');
-//         }
-//     };
-// };
+        if (this.workTime == 0 || this.workTime == undefined) {
+            console.log('Лампочка "' + this.name + '" не включена');
+        } else {
+            console.log('Лампочка "' + this.name + '" проработала - ' + this.workTime + 'ч, и стоимость потраченой электроэнергии составляет = ' + this.result + 'р.');
+        }
+    };
+};
  
-// let lightBulb = new Lightbulb('Кухня');
-// lightBulb.get();
+let lightBulb = new Lightbulb('Кухня');
+lightBulb.get();
  
-// let lightBulb2 = new Lightbulb('Коридор');
-// lightBulb2.get();
+let lightBulb2 = new Lightbulb('Коридор');
+lightBulb2.get();
  
-// let lightBulb3 = new Lightbulb('Ванная');
-// lightBulb3.get();
+let lightBulb3 = new Lightbulb('Ванная');
+lightBulb3.get();
  
-// let totalCost = lightBulb.result + lightBulb2.result + lightBulb3.result;
+let totalCost = lightBulb.result + lightBulb2.result + lightBulb3.result;
  
-// console.log('Всего стоимость потраченной электроэнергии составляет - ' + totalCost + 'р');
+console.log('Всего стоимость потраченной электроэнергии составляет - ' + totalCost + 'р');
 
 
 console.log('');
