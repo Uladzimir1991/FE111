7729767f35e2c2197c56b413ef36862012a69b32
Nestsiarovich Uladzimir <winner20021991@mail.ru>
2021-09-21 18:47:14 +0300

    Update work - JS__11

--- JS_11/script.js
+++ JS_11/script.js
@@ -67,6 +67,7 @@ class Contacts {
         if(index === null) return;
 
         this.data.splice(index, 1)
+
     }
 
     get() {
@@ -79,7 +80,16 @@ class ContactsApp extends Contacts {
 
     constructor() {
         super()
-        this.app();
+
+        if(!this.storage) {
+            this.getData().then(data => {
+                this.storage = data;
+
+                this.app();
+        })
+        } else {
+            this.app();
+        }
     }
 
     app() {
@@ -136,6 +146,95 @@ class ContactsApp extends Contacts {
         this.btnAdd.addEventListener('click' , event => {
             this.onAdd(event)
         })
+
+        let data = this.storage;
+
+        if(data && data.length > 0) {
+            this.data = data;
+            this.updateList();
+        }
+    }
+
+    async getData() {
+
+        return await fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+                .then(data => {
+                    data = data.map(user => {
+                        return {
+                            data: {
+                                id: user.id,
+                                name: user.name,
+                                email: user.email,
+                                address: user.address,
+                                phone: user.phone
+                            }
+                        }
+                    })
+            return data;
+        })
+
+    }
+
+    getCookie(name) {
+        let matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+    setCookie(name, value, options = {}) {
+        options = {
+            path: '/',
+            // при необходимости добавьте другие значения по умолчанию
+            ...options
+        };
+
+        if (options.expires instanceof Date) {
+            options.expires = options.expires.toUTCString();
+        }
+
+        let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+        for (let optionKey in options) {
+            updatedCookie += "; " + optionKey;
+            let optionValue = options[optionKey];
+            if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+            }
+        }
+
+        document.cookie = updatedCookie;
+    }
+
+    set storage(data) {
+
+        localStorage.setItem('users', JSON.stringify(data))
+        this.setCookie('storageExpiration', 'true', {'max-age': 10})
+
+    }
+
+    get storage() {
+
+        const storageExpiration = this.getCookie('storageExpiration')
+
+        if(!storageExpiration) localStorage.removeItem('users')
+
+
+        let data = localStorage.getItem('users')
+ 
+        if(!data || data.length == 0) return;
+
+        data = JSON.parse(data)
+
+        if(!data || data.length == 0) return;
+
+        data = data.map(user => {
+            user = new User(user.data)
+            return user;
+        })
+
+        return data;
     }
 
     updateList() {
@@ -162,18 +261,15 @@ class ContactsApp extends Contacts {
             elemEmail.classList.add('user__email');
 
             const elemAddress = document.createElement('div');
-            elemAddress.classList.add('user__email');
+            elemAddress.classList.add('user__address');
 
             const elemPhone = document.createElement('div');
-            elemPhone.classList.add('user__email');
+            elemPhone.classList.add('user__phone');
 
             const removeBtn = document.createElement('button');
             removeBtn.classList.add('user__remove');
             removeBtn.innerHTML = 'X';
 
-            const regexp8 = /\+?375 ?\(?\-?[0-9]{2}\)? ?\-?[1-9][0-9]+\-? ?[0-9]+\-? ?[0-9]{2}/gi; 
-
-            const regexp9 = /([\w]+[._-]?[\w]){2,25}@{1}[a-z]+[.-]?[a-z]+.[a-z]{2,11}/gi;
 
             if (user.data.name) h2.innerHTML = user.data.name;
             if (user.data.name == null) {
@@ -181,22 +277,19 @@ class ContactsApp extends Contacts {
                 return;
             }
 
-            if (regexp9.test(user.data.email)) elemEmail.innerHTML = user.data.email;
-
-            if (user.data.address) elemAddress.innerHTML = user.data.address;
-
-            if(!regexp8.test(user.data.phone)) {
+            if (user.data.email) elemEmail.innerHTML = user.data.email;
+            
+            if (user.data.address) {
+                const address = 'city:'+ user.data.address.city + ' ' + 'street:'+ user.data.address.street;
+                elemAddress.innerHTML = address;
+            }
+            
+            if(user.data.phone == null) {
                 user.data.id -= 1;
                 return;
             }
 
-            if (regexp8.test(user.data.phone)) elemPhone.innerHTML = user.data.phone;
-            console.log(elemPhone.innerHTML = user.data.phone)
-
-            if((user.data.name != null && !regexp8.test(user.data.phone)) || (user.data.name == null && regexp8.test(user.data.phone))) {
-                user.data.id -= 1;
-                return;
-            }
+            if (user.data.phone) elemPhone.innerHTML = user.data.phone;
 
                         // Контейнер с иконками
             const icoMessengers = document.createElement('div')
@@ -206,15 +299,76 @@ class ContactsApp extends Contacts {
             skype.classList.add('ico__skype')
             skype.src = "/images/skype.png"
 
+            const aSkype = document.createElement('a')
+            aSkype.href = `skype: ${user.data.name}?call`;
+
             const viber = document.createElement('img')
             viber.classList.add('ico__viber')
             viber.src = "/images/viber.png"
+
+            const aViber = document.createElement('a')
+            aViber.href = `tel: ${user.data.phone}`;
 
             const phone = document.createElement('img')
             phone.classList.add('ico__phone')
             phone.src = "/images/phone.png"
 
-            icoMessengers.append(skype, viber, phone)
+            const aPhone = document.createElement('a')
+            aPhone.href = `tel: ${user.data.phone}`;
+
+            aSkype.append(skype)
+            aPhone.append(phone)
+            aViber.append(viber)
+            icoMessengers.append(aSkype, aViber, aPhone)
+
+            
+            // Контейнер с иконками телефонов
+            const getPhone = async function() {
+
+                let response = await fetch('https://api.github.com/emojis')
+                if(!response.ok) return;
+            
+                let data = await response.json();
+
+                let dataIphone = data.iphone,
+                    dataHomePhone = data.phone;
+
+                const phoneBlock = document.createElement('div')
+                phoneBlock.classList.add('phone')
+                phoneBlock.innerHTML = `
+                    <img id="id" class="homePhone" src="${dataHomePhone}"/>
+                    <img id="id2" class="mobilePhone" src="${dataIphone}"/>
+                `
+
+                const arrowRight = document.createElement('span')
+                arrowRight.classList.add('arrowRight')
+                arrowRight.innerHTML = '>'
+
+                arrowRight.addEventListener('click', event => {
+                    if(!event.target) return;
+
+                    phoneBlock.innerHTML = `
+                        <img class="homePhone homePhone__hidden" src="${dataHomePhone}"/>
+                        <img class="mobilePhone mobilePhone__visible" src="${dataIphone}"/>
+                    `
+                })
+
+                const arrowLeft = document.createElement('span')
+                arrowLeft.classList.add('arrowLeft')
+                arrowLeft.innerHTML = '<'
+
+                arrowLeft.addEventListener('click', event => {
+                    if(!event.target) return;
+
+                    phoneBlock.innerHTML = `
+                        <img class="homePhone" src="${dataHomePhone}"/>
+                        <img class="mobilePhone" src="${dataIphone}"/>
+                    `
+                })
+
+                liElems.append(phoneBlock, arrowRight, arrowLeft);
+
+            }();
 
 
             liElems.append(h2, elemEmail, elemAddress, elemPhone, removeBtn);
@@ -264,6 +418,9 @@ class ContactsApp extends Contacts {
             });
 
         });
+
+        this.storage = data;
+
     }
     
 
@@ -293,7 +450,8 @@ class ContactsApp extends Contacts {
         this.elemAddress.value = '';
         this.elemPhone.value = '';
 
-        this.add(data);
+        this.storage = data;
+        this.add(data);          
         this.updateList();
 
     }
@@ -306,10 +464,11 @@ class ContactsApp extends Contacts {
         let data = {};
 
         data[key] = event.target.textContent;
-        
+
         this.edit(id, data);
         this.updateList();
         event.target.setAttribute('contenteditable', false);
+
     }
 
     onRemove(event, id) {
@@ -317,27 +476,8 @@ class ContactsApp extends Contacts {
         this.updateList();
     }
 
-
-    // get storage() {
-            // return window.localStorage.getItem(this.data);
-    // }
-
-    // set storage() {
-    // 
-    // }
-
-
-
 }
 
-    //     В классе «ContactsApp» реализуйте сл. св-ва и методы:
-    // 1. Геттер и сеттер «storage()» для хранения (добавления и обновления) и получения данных из
-    // localStorage браузера.
-    // 2. При каждом обновлении localStorage должен создаваться COOKIE параметр
-    // «storageExpiration» на уровне всего приложения и сроком на 10 дней.
-    // 3. По истечению срока действия «storageExpiration» все данных из localStorage и COOKIES
-    // должны быть удалены.
-    // Вы можете добавлять любые другие свойства и методы во все классы.
 window.addEventListener('DOMContentLoaded', function() {
         new ContactsApp();
 })
