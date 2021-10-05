26ec90cbb2ec74ee671027a38ed2fd72d31d0f53
Nestsiarovich Uladzimir <winner20021991@mail.ru>
2021-10-05 20:16:42 +0300

    update js_14

--- JS_14/Pages/Cart.js
+++
@@ -1,5 +0,0 @@
-const title = 'Cart! Cart here, cart there... Cart everywhere'
-
-const content = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quam error deserunt suscipit ad repellat assumenda blanditiis reiciendis impedit amet aperiam, qui natus, magni rerum fugiat placeat deleniti laborum quos.'
-
-export {title, content}
--- JS_14/Pages/Delivery.js
+++
@@ -1,5 +0,0 @@
-const title = 'Delivery terms!'
-
-const content = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente quam error deserunt suscipit ad repellat assumenda blanditiis reiciendis impedit amet aperiam, qui natus, magni rerum fugiat placeat deleniti laborum quos.'
-
-export {title, content}
